4e04cfec04e25825eb22fd6282910809e4f1cbcc
Stepichev Oleg <Leganyst@yandex.ru>
2024-10-02 20:14:45 +0300

    Отключен CORS

--- app/init.py
+++ app/init.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.user.register import register_router
 from app.routes.user.auth import auth_router
 from app.routes.user.update_user import update_user_router
 
 app = FastAPI()
+
+# Разрешаем все CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем все источники
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все HTTP методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
 
 app.include_router(register_router)
 app.include_router(auth_router)
